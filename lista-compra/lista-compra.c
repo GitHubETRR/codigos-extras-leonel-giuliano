36eb3536c45e6e96928fb18ec1afcc5718bdf23f
leonel-giuliano <129548024+leonel-giuliano@users.noreply.github.com>
2024-05-02 11:41:16 -0300

    Eliminación de includes repetidos

--- lista-compra/lista-compra.c
+++ lista-compra/lista-compra.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <string.h>
-#include <stdlib.h>
 
 #include "./lista-compra.h"
 #include "../ascii/ascii.h"
--- lista-compra/main.c
+++ lista-compra/main.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-#include <stdint.h>
-#include <string.h>
-#include <stdlib.h>
 
 #include "./lista-compra.h"
 
