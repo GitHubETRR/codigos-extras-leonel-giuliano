613287fd99f9bfad8b91acbb9e6b0eb61c34f652
leonel-giuliano <leonelgiuliano7@gmail.com>
2024-05-04 03:35:13 -0300

    Carácter ' ' (255) para poder identificar productos

--- lista-compra/file.c
+++ lista-compra/file.c
@@ -6,6 +6,6 @@ void fileAddProduct(product_t *newProduct, FILE **fileList) {
     fseek(*fileList, 0L, SEEK_END);
 
     fputs("     -----     PRODUCTO     -----\n", *fileList);
-    fprintf(*fileList, "Producto: %s\n", newProduct->name);
+    fprintf(*fileList, "Producto: %s\n", newProduct->name);
     fprintf(*fileList, "Cantidad a comprar: %d\n\n", newProduct->cant);
 }
