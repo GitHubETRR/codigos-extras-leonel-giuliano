ff8554d277bb4769f8f22eaa2a697b7b35f28d95
leonel-giuliano <129548024+leonel-giuliano@users.noreply.github.com>
2024-05-13 09:32:54 -0300

    Setting the files

--- update-code/main.c
+++ update-code/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 
 #include "error.h"
 #include "mallist.h"
@@ -14,6 +15,11 @@ enum {
 
 int main(int argc, char *argv[]) {
     if(argc != ARGC_PARAMETERS) errorHandler(ERROR_ARGC);
+
+    FILE *lastFile, *newFile, *output;
+    if((lastFile = fopenList(argv[ARGV_FILE], "r")) == NULL) errorHandler(ERROR_FILE);
+    if((newFile = fopenList(argv[ARGV_NEW_FILE], "r")) == NULL) errorHandler(ERROR_FILE);
+    if((output = fopenList(argv[ARGV_OUTPUT], "w")) == NULL) errorHandler(ERROR_FILE);
 
     return 0;
 }
